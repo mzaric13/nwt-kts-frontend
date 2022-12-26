07ffe855f283d1e8188ed73745fe1d7d7129f9b6
Zoran Bukorac <zoran00.zb@gmail.com>
2022-12-26 18:01:48 +0100

    Add autocomplete address search

--- src/app/components/route-form/route-form.component.ts
+++ src/app/components/route-form/route-form.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import OpenStreetMapProvider from 'leaflet-geosearch/lib/providers/openStreetMapProvider';
 import * as locationsJson from '../../../assets/locations.json';
 
 @Component({
@@ -14,28 +13,17 @@ export class RouteFormComponent implements OnInit {
     destination: new FormControl(''),
   });
 
-  private pickupCoordinates!: number[];
-  private destinationCoordinates!: number[];
-
-  results!: string[];
+  public locations: string[] = Object.values(locationsJson);
 
   public showDropdownPickup: boolean = false;
 
   public showDropdownDestination: boolean = false;
-
-  private provider!: OpenStreetMapProvider;
 
   @Output() makeRoute = new EventEmitter();
 
   constructor() {}
 
   private initGeoCodeSearch() {
-    this.provider = new OpenStreetMapProvider({
-      params: {
-        countryCodes: 'rs',
-      },
-    });
-
     this.locations.splice(this.locations.length - 2, 2);
   }
 
@@ -44,18 +32,6 @@ export class RouteFormComponent implements OnInit {
   }
 
   onSubmit() {
-    const a: string = this.route.controls.pickup.value!;
-    let labels: string[];
-    this.provider.search({ query: a }).then((result) => {
-      console.log(result[0].x, result[0].y);
-      labels = result.map((r) => r.label);
-      this.results = labels.map((label) =>
-        label
-          .split(',')
-          .slice(0, labels.length - 5)
-          .join('')
-      );
-    });
     this.makeRoute.emit([
       this.route.controls.pickup.value,
       this.route.controls.destination.value,
@@ -95,6 +71,4 @@ export class RouteFormComponent implements OnInit {
   getDestination(): string {
     return this.route.controls.destination.value!;
   }
-
-  public locations: string[] = Object.values(locationsJson);
 }
