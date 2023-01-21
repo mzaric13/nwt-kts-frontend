d7a68f10bde362e77da4c44c23bb3c5ee4599f91
ColdBubbyGuy <sakal.teodor@gmail.com>
2023-01-22 00:17:15 +0100

    bugfix

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { url } from 'vuelidate/lib/validators';
 import { MessageDTO } from '../models/message-dto';
 import { TokenService } from './token.service';
 
