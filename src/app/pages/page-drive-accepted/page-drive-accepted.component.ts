7a39abc0402fd40c28e37ed09a9e0fb4d28b608d
Zoran Bukorac <zoran00.zb@gmail.com>
2023-01-24 15:56:57 +0100

    Add swal alert to accepted drive page

--- src/app/pages/page-drive-accepted/page-drive-accepted.component.ts
+++ src/app/pages/page-drive-accepted/page-drive-accepted.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DriveService } from 'src/app/services/drive.service';
 import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-page-drive-accepted',
@@ -10,13 +11,23 @@ import { ActivatedRoute } from '@angular/router';
 export class PageDriveAcceptedComponent implements OnInit {
 
   constructor(private readonly driveService: DriveService,
-  private route: ActivatedRoute) { }
+    private route: ActivatedRoute) { }
+  
+  okStatus: boolean = false;
 
   ngOnInit(): void {
     this.route.queryParams.subscribe(params => {
       this.driveService.acceptDriveConsent(params['tempDriveId']).subscribe({
         next: () => {
-          
+          this.okStatus = true;
+        },
+        error: (err) => {
+          console.log(err);
+          Swal.fire({
+            icon: 'error',
+            title: 'error!',
+            text: err.error.apierror.message,
+          });
         }
       })
     })
